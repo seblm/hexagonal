50b60e8a17cafd2152c11ec91de7c8928f4b4ad1
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-04-15 01:54:44 +0200

    🔊 add debug logs when sending to Slack

--- src/main/scala/meal/Meals.scala
+++ src/main/scala/meal/Meals.scala
@@ -4,8 +4,11 @@ import java.nio.file.Paths
 import java.time.LocalDate
 
 import meal.FileMealsRepository.{AddError, ListError, RemoveError}
+import org.slf4j.{Logger, LoggerFactory}
 
 class Meals {
+
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
 
   private val mealsRepository = new FileMealsRepository(Paths.get("meals.txt"))
 
@@ -16,10 +19,11 @@ class Meals {
   def add(date: LocalDate, description: String): Either[AddError, Meal] = {
     val addResult = mealsRepository.add(Meal(date, description))
 
-    addResult match {
+    val sendResult = addResult match {
       case Left(error) => slackMealClient.send(s"wow impossible d'ajouter le plat _${description}_ le $date: $error")
       case Right(_)    => slackMealClient.send(s"tu as mangé le plat _${description}_ le $date")
     }
+    logger.debug(s"add: $sendResult")
 
     addResult
   }
@@ -27,12 +31,13 @@ class Meals {
   def remove(date: LocalDate): Either[RemoveError, Seq[Meal]] = {
     val removeResult = mealsRepository.remove(date)
 
-    removeResult match {
+    val sendResult = removeResult match {
       case Left(error)         => slackMealClient.send(s"wow impossible de supprimer le $date: $error")
       case Right(Nil)          => slackMealClient.send(s"aucun repas à supprimer pour le $date")
       case Right(_ :: Nil)     => slackMealClient.send(s"un repas supprimé pour le $date")
       case Right(removedMeals) => slackMealClient.send(s"${removedMeals.length} repas supprimés pour le $date")
     }
+    logger.debug(s"remove: $sendResult")
 
     removeResult
   }
